8630703491b56f9219cb1d69d89f18c8a997e90e
briceshun <bricevalentinshun@gmail.com>
2025-12-31 16:03:11 +1300

    Update topic_list in constants.py to include a comprehensive range of categories for better classification

--- constants.py
+++ constants.py
@@ -1,3 +1,22 @@
 plutchik = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust']
-topic_list = ['damages', 'hurricane advice', 'hurricane relief services', 'personal opinion', 'weather information', 'other']
+topic_list = [
+    'Conspiracy and Historical Events',
+    'Disaster and Resilience',
+    'Disputes and Opinions',
+    'Economic and Financial Matters',
+    'Education and Knowledge',
+    'Environmental and Infrastructure',
+    'Health and Social Welfare',
+    'Labor and Workforce',
+    'Leadership and Governance',
+    'Legal and Regulatory Framework',
+    'Miscellaneous and Other',
+    'Power and Influence Dynamics',
+    'Psychological and Emotional Aspects',
+    'Quality of Life and Standards',
+    'Security and Conflict',
+    'Social and Cultural Aspects',
+    'Sports and Recreation',
+    'Trust, Credibility, and Communication',
+]
 path = r"/Users/briceshun/Documents/PhD 2022/youtube-emotion-foraging/data/parquet/"
